1e296a48a169bfeaf9383e678cbdf455c92a662f
shaun <ycorredius@gmail.com>
2020-05-05 11:32:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 // write your createStore function here
+function createStore(candyReducer){
+    let state;
+
+    function dispatch(action){
+      state = candyReducer(state,action);
+      render();
+    }
+
+    function getState() {
+      return state;
+    }
+
+    return{
+      getState,
+      dispatch
+    }
+}
+
+let store = createStore(candyReducer);
+store.dispatch({type: '@@INIT'});
 
 function candyReducer(state = [], action) {
   switch (action.type) {
